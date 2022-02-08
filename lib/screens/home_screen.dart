dad0c91d4a888467d4fb55d2fdb1aec9e4f2fb84
hirorock1103 <mdiz1103@gmail.com>
2022-02-08 13:36:35 +0900

    微調整

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -15,7 +15,9 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final picker = ImagePicker();
   String msg = "ここに画像がセットされます";
-  late File _image ;
+  String resultMsg = "結果が表示されます";
+  File? _image = null;
+
   //NewsListViewModel viewModel = NewsListViewModel();
 
   @override
@@ -36,37 +38,49 @@ class _HomeScreenState extends State<HomeScreen> {
                     onPressed: () async {
                       //カメラ起動して撮影→撮影後画像補正画面となる
                       getImageFromCamera();
+                      msg = "カメラ起動";
                     },
                     child: Text("カメラ起動")),
                 ElevatedButton(
                     onPressed: () async {
                       //カメラ起動して撮影→撮影後画像補正画面となる
                       getImageFromGallery();
+                      msg = "ライブラリ起動";
                     },
                     child: Text("ギャラリー選択")),
               ],
             ),
             Text(msg),
             Expanded(
+                flex: 2,
                 child: Container(
-              decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
-              child: _image == null
-                  ? Center(child: Text("撮影画像を表示"))
-                  : Image.file(_image),
-            )),
+                  decoration:
+                      BoxDecoration(border: Border.all(color: Colors.grey)),
+                  child: _image == null
+                      ? Center(child: Text("撮影画像を表示"))
+                      : Image.file(_image!),
+                )),
+            Expanded(
+              flex: 1,
+              child: Container(
+                child: Column(
+                  children: [
+                    Text("- result -"),
+                    Text(resultMsg),
+                  ],
+                ),
+              ),
+            ),
             ElevatedButton(
                 onPressed: () async {
                   //OCR送信 apiで通信をする
 
-                   //List<NewsItem> list =  await viewModel.getNews();
+                  //List<NewsItem> list =  await viewModel.getNews();
                   // list.forEach((element) {
                   // });
                   setState(() {
-
+                    msg = "OCR　API実行";
                   });
-
-
-
                 },
                 child: Text("OCR"))
           ],
